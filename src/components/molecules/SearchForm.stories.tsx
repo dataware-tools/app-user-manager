b08aa2e0b5fa52a5267f1369a11d6fe01347e040
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-04-02 15:31:15 +0900

    add search form

---
+++ src/components/molecules/SearchForm.stories.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { SearchForm, SearchPropType } from "./SearchForm";
+import { Story } from "@storybook/react";
+
+export default {
+  component: SearchForm,
+  title: "SearchForm",
+};
+
+const Template: Story<SearchPropType> = (args) => <SearchForm {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  onSearch: () => {},
+  onSearchTextChange: () => {},
+  searchText: "",
+};
+
+const ComponentChangable = (): JSX.Element => {
+  const [searchText, setSearchText] = React.useState("change this text");
+  const onSearch = () => {
+    console.log(searchText);
+  };
+  const onSearchTextChange = (newSearchText: string) => {
+    setSearchText(newSearchText);
+  };
+  console.log(searchText);
+
+  return (
+    <SearchForm
+      searchText={searchText}
+      onSubmit={onSearch}
+      onChange={onSearchTextChange}
+    />
+  );
+};
+
+export const Changable = (): JSX.Element => <ComponentChangable />;
