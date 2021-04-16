2f16eea6d2ad58d3390455a6acbb178c9e31b196
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-04-16 10:12:33 +0900

    save tab name as path param

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,66 @@ export function isNonNullable<T>(val: T): val is NonNullable<T> {
   return val !== null && val !== undefined;
 }
 
+export function getURLParam(param: string): string | null {
+  return new URLSearchParams(window.location.search).get(param);
+}
+
+export type PathParamConvertibleObj = Record<
+  string,
+  string | number | undefined | null
+>;
+
+export function ObjToParamString(obj: PathParamConvertibleObj): string {
+  let flag = true;
+  let paramString = "";
+  for (const [key, value] of Object.entries(obj)) {
+    if (flag && value) {
+      paramString += `?${key}=${value}`;
+      flag = false;
+    } else if (value) {
+      paramString += `&${key}=${value}`;
+    }
+  }
+  return paramString;
+}
+
+export function addParamToURL(
+  newParam: string | PathParamConvertibleObj,
+  method: "push" | "replace"
+): void {
+  if (newParam === "" || newParam === "?" || newParam === "&") {
+    return;
+  }
+  const currentParamObj = new URLSearchParams(window.location.search);
+  const newParamObj = new URLSearchParams(
+    typeof newParam === "string" ? newParam : ObjToParamString(newParam)
+  );
+
+  for (const key of newParamObj.keys()) {
+    if (currentParamObj.has(key)) {
+      currentParamObj.delete(key);
+    }
+  }
+
+  const currentParamString = currentParamObj.toString();
+  const newParamString = newParamObj.toString();
+  const newRelativeURL =
+    currentParamString === "" && newParamString === ""
+      ? ""
+      : currentParamString === "" && newParamString !== ""
+      ? `?${newParamString}`
+      : currentParamString !== "" && newParamString === ""
+      ? `?${currentParamString}`
+      : `?${currentParamString}&${newParamString}`;
+  if (method === "push") {
+    console.log("pushed!");
+    history.pushState(null, "", newRelativeURL);
+  } else if (method === "replace") {
+    console.log("replaced!");
+    history.replaceState(null, "", newRelativeURL);
+  }
+}
+
 export const APP_ROUTE = {
   HOME: "/",
 };
