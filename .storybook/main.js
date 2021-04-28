f4d237e6f3ab9a02ed4e2117868983226bcbd6b9
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-04-28 14:42:31 +0900

    fix storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,6 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-  webpackFinal: async (baseConfig) => {
-    const nextConfig = require("../next.config.js");
-
-    return { ...baseConfig, ...nextConfig };
-  },
   typescript: {
     check: false,
     checkOptions: {},
@@ -15,5 +10,17 @@ module.exports = {
       propFilter: (prop) =>
         prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
     },
+  },
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // See: https://storybook.js.org/docs/react/workflows/faq#how-do-i-setup-storybook-to-share-webpack-configuration-with-nextjs
+    // See: https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
+    const nextConfig = require("../next.config.js");
+
+    if (nextConfig.webpack) return { ...config, ...nextConfig.webpack() };
+    else return config;
   },
 };
