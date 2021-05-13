ddef74b6a43d3310018a8f176f06318915721085
yuri-sakai <yuri.sakai1019@gmail.com>
2021-05-13 18:37:45 +0900

    fix parpage component

--- src/components/molecules/PerPageSelect.tsx
+++ src/components/molecules/PerPageSelect.tsx
@@ -1,38 +1,42 @@
-import React from 'react';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
+import React from "react";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
 
-const PerPageSelect = (): JSX.Element => {
-  const [page, setPage] = React.useState('20');
-
+type PropsType = { perPage: number; setPerPage: (perPage: number) => void };
+const PerPageSelect = ({ perPage, setPerPage }: PropsType): JSX.Element => {
   const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setPage(event.target.value as string);
+    setPerPage(event.target.value as number);
   };
 
   return (
-    <div>
-      <FormControl>
-        <Select
-          labelId="perPage"
-          value={page}
-          onChange={handleChange}
-          displayEmpty
-          inputProps={{ 'aria-label': 'Without label' }}
-        >
-          <MenuItem value="" disabled>
-            perPage
-          </MenuItem>
-          <MenuItem value={20}>20</MenuItem>
-          <MenuItem value={50}>50</MenuItem>
-          <MenuItem value={100}>100</MenuItem>
-          <MenuItem value={200}>200</MenuItem>
-          <MenuItem value={500}>500</MenuItem>
-        </Select>
-        <FormHelperText>perPage</FormHelperText>
-        </FormControl>
-
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginRight: "5px",
+        }}
+      >
+        perPage
+      </div>
+      <Select
+        labelId="perPage"
+        value={perPage}
+        onChange={handleChange}
+        displayEmpty
+        inputProps={{ "aria-label": "Without label" }}
+        variant="standard"
+      >
+        <MenuItem value="" disabled>
+          perPage
+        </MenuItem>
+        <MenuItem value={20}>20</MenuItem>
+        <MenuItem value={50}>50</MenuItem>
+        <MenuItem value={100}>100</MenuItem>
+        <MenuItem value={200}>200</MenuItem>
+        <MenuItem value={500}>500</MenuItem>
+      </Select>
     </div>
   );
 };
