1a46d58d58ecb5abd0bbd69111eae4e50b2bf89f
d-hayashi <daiki@d-hayashi.me>
2021-06-08 11:39:05 +0900

    Add headers to UserList

--- src/components/organisms/UserList.tsx
+++ src/components/organisms/UserList.tsx
@@ -1,6 +1,12 @@
-import { permissionManager } from "@dataware-tools/app-common";
-import { UserListItem, UserListItemProps } from "./UserListItem";
-import { useMemo, RefObject } from "react";
+import {permissionManager} from "@dataware-tools/app-common";
+import {UserListItem, UserListItemProps} from "./UserListItem";
+import {useMemo, RefObject} from "react";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import TableBody from "@material-ui/core/TableBody";
+import {makeStyles} from "@material-ui/core/styles";
 
 type Users = permissionManager.UserModel[];
 type Roles = permissionManager.RoleModel[];
@@ -11,12 +17,18 @@ type UserListProps = {
   onUpdateUser: UserListItemProps["onUpdateUser"];
 };
 
+const useStyles = makeStyles({
+  headerCell: {
+    height: "47px"
+  }
+});
+
 const UserList = ({
-  users,
-  roles,
-  bottomRef,
-  onUpdateUser,
-}: UserListProps): JSX.Element => {
+                    users,
+                    roles,
+                    bottomRef,
+                    onUpdateUser,
+                  }: UserListProps): JSX.Element => {
   const fixedRoles = useMemo(() => {
     return roles.map((role) => ({
       name: role.name,
@@ -24,20 +36,32 @@ const UserList = ({
     }));
   }, [roles]);
 
+  const styles = useStyles();
+
   return (
-    <div>
-      {users.map((user) => (
-        <UserListItem
-          user={user}
-          key={user.user_id}
-          roles={fixedRoles}
-          onUpdateUser={onUpdateUser}
-        />
-      ))}
-      <div ref={bottomRef} />
-    </div>
+    <>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell key="user" className={styles.headerCell}>User</TableCell>
+            <TableCell key="roles" className={styles.headerCell}>Roles</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {users.map((user) => (
+            <UserListItem
+              user={user}
+              key={user.user_id}
+              roles={fixedRoles}
+              onUpdateUser={onUpdateUser}
+            />
+          ))}
+        </TableBody>
+      </Table>
+      <div ref={bottomRef}/>
+    </>
   );
 };
 
-export { UserList };
-export type { UserListProps };
+export {UserList};
+export type {UserListProps};
