ea26cd3325c08a8e27819a58cea5843936098ab4
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-06-24 12:16:17 +0900

    Separate logic and view of PremissionList

--- src/components/organisms/PermissionList.tsx
+++ src/components/organisms/PermissionList.tsx
@@ -4,34 +4,32 @@ import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import { PermissionListItem } from "./PermissionListItem";
-import { createRef } from "react";
+import {
+  PermissionListItem,
+  PermissionListItemProps,
+} from "./PermissionListItem";
+import { createRef, ReactNode, RefObject } from "react";
 import { AddCircle } from "@material-ui/icons";
 import {
   permissionManager,
   metaStore,
   theme as themeInstance,
   Spacer,
-  NoticeableLetters,
+  SquareIconButton,
 } from "@dataware-tools/app-common";
 
 const useStyles = makeStyles((theme: typeof themeInstance) => ({
-  title: {
-    fontSize: "1.5rem",
-    fontWeight: "bold",
-  },
   header: {
     fontSize: "1.2rem",
     fontWeight: "bold",
     lineHeight: 1.7,
   },
   tableContainer: {
-    // TODO: media query
-    height: "35vh",
+    height: "30vh",
     margin: "0 3vw",
     overflowY: "auto",
   },
-  tableTop: {
+  titleContainer: {
     alignItems: "center",
     display: "flex",
     padding: "10px 0",
@@ -53,68 +51,48 @@ const useStyles = makeStyles((theme: typeof themeInstance) => ({
 
 type Action = permissionManager.ActionModel;
 type Database = metaStore.DatabaseModel;
-
 type Permission = permissionManager.RoleModel["permissions"][number];
-type PermissionListProps = {
+
+type Props = {
+  classes: ReturnType<typeof useStyles>;
+  onItemChange: PermissionListItemProps["onChange"];
+  onItemDelete: PermissionListItemProps["onDelete"];
+  onItemAdd: () => void;
+  databaseNames: string[];
+  listBottomRef: RefObject<HTMLDivElement>;
+} & Omit<ContainerProps, "onChange" | "databases">;
+type ContainerProps = {
+  title: ReactNode;
   actions: Action[];
   databases: Database[];
   onChange: (newValue: Permission[]) => void;
   permissions: Permission[];
 };
 
-const PermissionList = ({
+const Component = ({
+  title,
+  classes,
   actions,
-  databases,
-  onChange,
+  databaseNames,
   permissions,
-}: PermissionListProps): JSX.Element => {
-  const styles = useStyles();
-
-  const listContainerRef = createRef<HTMLDivElement>();
-  const listBottomRef = createRef<HTMLDivElement>();
-
-  const scrollToBottomOfList = () => {
-    if (listBottomRef && listBottomRef.current) {
-      listBottomRef.current.scrollIntoView({
-        behavior: "smooth",
-      });
-    }
-  };
-
-  const fixedDatabases = databases
-    // TODO: fix API type? 名前を必ず返すなら name は required にする.
-    .filter((database) => Boolean(database.name))
-    .map((database) => database.name as NonNullable<typeof database.name>);
-
+  onItemChange,
+  onItemDelete,
+  onItemAdd,
+  listBottomRef,
+}: Props): JSX.Element => {
   return (
     <div>
-      <div className={styles.tableTop}>
-        <div className={styles.title}>
-          <NoticeableLetters>Permissions</NoticeableLetters>
-        </div>
+      <div className={classes.titleContainer}>
+        {title}
         <Spacer direction="horizontal" size="5px" />
-        <div
-          onClick={() => {
-            const newPermissions = [...permissions];
-            newPermissions.push({ databases: [], actions: [] });
-            onChange(newPermissions);
-            scrollToBottomOfList();
-          }}
-          className={styles.addButton}
-        >
-          <AddCircle />
-        </div>
+        <SquareIconButton icon={<AddCircle />} onClick={onItemAdd} />
       </div>
-      <div className={styles.tableContainer} ref={listContainerRef}>
+      <div className={classes.tableContainer}>
         <Table stickyHeader size="small">
           <TableHead>
             <TableRow>
-              <TableCell>
-                <div className={styles.header}>Databases</div>
-              </TableCell>
-              <TableCell>
-                <div className={styles.header}>Actions</div>
-              </TableCell>
+              <TableCell>Databases</TableCell>
+              <TableCell>Actions</TableCell>
               <TableCell />
             </TableRow>
           </TableHead>
@@ -126,33 +104,78 @@ const PermissionList = ({
                   permission={permission}
                   index={index}
                   actions={actions}
-                  databases={fixedDatabases}
-                  onChange={(targetIndex, newValue) => {
-                    const newPermissions = permissions.map((permission, i) =>
-                      i === targetIndex ? newValue : permission
-                    );
-                    onChange(newPermissions);
-                  }}
-                  onDelete={(targetIndex) => {
-                    const newPermissions = permissions.filter(
-                      (_, i) => i !== targetIndex
-                    );
-                    onChange(newPermissions);
-                  }}
+                  databases={databaseNames}
+                  onChange={onItemChange}
+                  onDelete={onItemDelete}
                 />
               );
             })}
           </TableBody>
         </Table>
-        {/* FIXME: state が非同期で更新されるので追加された permission までスクロールしないというを解決しようとしてこうなっています
-              より良い案を募集してます
-              そもそも末尾に追加するのではなく，先頭に追加するべき？ */}
-        <div className={styles.bottomSpace} />
+        {/* FIXME: View port will not contain new permission by executing scrollToBottomOfList, because state will be changed asynchronously.
+            So below extra space is needed to scroll to new permission, I think, however there may be a better way
+            Do yo have any other method?  */}
+        <div className={classes.bottomSpace} />
         <div ref={listBottomRef} />
       </div>
     </div>
   );
 };
+const Container = ({
+  actions,
+  databases,
+  onChange,
+  permissions,
+  ...delegated
+}: ContainerProps): JSX.Element => {
+  const classes = useStyles();
+  const listBottomRef = createRef<HTMLDivElement>();
 
-export { PermissionList };
-export type { PermissionListProps };
+  const scrollToBottomOfList = () => {
+    if (listBottomRef && listBottomRef.current) {
+      listBottomRef.current.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const databaseNames = databases
+    .filter((database) => Boolean(database.name))
+    .map((database) => database.name as NonNullable<typeof database.name>);
+
+  const onItemChange: Props["onItemChange"] = (targetIndex, newValue) => {
+    const newPermissions = permissions.map((permission, i) =>
+      i === targetIndex ? newValue : permission
+    );
+    onChange(newPermissions);
+  };
+
+  const onItemDelete: Props["onItemDelete"] = (targetIndex) => {
+    const newPermissions = permissions.filter((_, i) => i !== targetIndex);
+    onChange(newPermissions);
+  };
+
+  const onItemAdd: Props["onItemAdd"] = () => {
+    const newPermissions = [...permissions];
+    newPermissions.push({ databases: [], actions: [] });
+    onChange(newPermissions);
+    scrollToBottomOfList();
+  };
+
+  return (
+    <Component
+      classes={classes}
+      actions={actions}
+      permissions={permissions}
+      databaseNames={databaseNames}
+      onItemChange={onItemChange}
+      onItemDelete={onItemDelete}
+      onItemAdd={onItemAdd}
+      listBottomRef={listBottomRef}
+      {...delegated}
+    />
+  );
+};
+
+export { Container as PermissionList };
+export type { ContainerProps as PermissionListProps };
