b30ce9bb83bbe5f711c87c6bf655e4a71cc62799
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-06-24 20:08:05 +0900

    Add usePrevious

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,20 @@
-export const APP_ROUTE = {
+import { useRef, useEffect } from "react";
+const APP_ROUTE = {
   HOME: "/",
 };
 
-export const SwrOptions = {
+const SwrOptions = {
   errorRetryCount: 1,
 };
+
+const usePrevious = <T>(value: T): T | undefined => {
+  const ref = useRef<T>();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+};
+
+export { APP_ROUTE, SwrOptions, usePrevious };
+export * from "./utilTypes";
+export * from "./fetchClient";
