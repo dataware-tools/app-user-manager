922ea7fb70e3330af261d0cf804b125e286fb4db
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-06-28 19:38:05 +0900

    Separate logic and view of UsersPage

--- src/components/organisms/UsersPage.tsx
+++ src/components/organisms/UsersPage.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import useSWR, { mutate } from "swr";
+import { mutate } from "swr";
 import {
   permissionManager,
   LoadingIndicator,
@@ -11,10 +11,10 @@ import {
   PageToolBar,
   Spacer,
   getQueryString,
-  API_ROUTE,
-  objToQueryString,
   addQueryString,
   PageMain,
+  SearchFormProps,
+  PerPageSelectProps,
 } from "@dataware-tools/app-common";
 
 import { makeStyles } from "@material-ui/core/styles";
@@ -23,131 +23,55 @@ import Button from "@material-ui/core/Button";
 import Pagination from "@material-ui/core/Pagination";
 
 import { UserList, UserListProps } from "./UserList";
+import { useListUsers, useListRoles, fetchPermissionManager } from "utils";
 
-const useStyles = makeStyles(() => ({
-  paginationContainer: {
-    alignItems: "center",
-    display: "flex",
-    justifyContent: "center",
-  },
-}));
-const Container = (): JSX.Element => {
-  // TODO: save per_page to local state
-  const [searchText, setSearchText] = useState(
-    getQueryString("searchText") || ""
-  );
-  const [perPage, setPerPage] = useState(
-    Number(getQueryString("perPage")) || 20
-  );
-  const [page, setPage] = useState(Number(getQueryString("page")) || 1);
-  const { getAccessTokenSilently } = useAuth0();
-  const [error, setError] = useState<null | ErrorMessageProps>(null);
+type Props = {
+  classes: ReturnType<typeof useStyles>;
+  error?: ErrorMessageProps;
+  searchText: SearchFormProps["defaultValue"];
+  perPage: PerPageSelectProps["perPage"];
+  perPageOptions: PerPageSelectProps["values"];
+  users?: UserListProps["users"];
+  roles?: UserListProps["roles"];
+  isFetchComplete: boolean;
+  totalPage: number;
+  page: number;
+  onChangeSearchText: SearchFormProps["onSearch"];
+  onChangePerPage: PerPageSelectProps["setPerPage"];
+  onSaveUser: UserListProps["onUpdateUser"];
+  onChangePage: (newPage: number) => void;
+};
 
-  const styles = useStyles();
-
-  const listUsers = async () => {
-    permissionManager.OpenAPI.TOKEN = await getAccessTokenSilently();
-    permissionManager.OpenAPI.BASE = API_ROUTE.PERMISSION.BASE;
-    const listUsersRes = await permissionManager.UserService.listUsers({
-      perPage: perPage,
-      page: page,
-      search: searchText,
-    });
-    return listUsersRes;
-  };
-
-  const listUsersQuery = objToQueryString({
-    per_page: perPage,
-    page: page,
-    search_text: searchText,
-  });
-  const listUsersURL = `${API_ROUTE.PERMISSION.BASE}/users${listUsersQuery}`;
-
-  const { data: listUsersRes, error: listUsersError } = useSWR(
-    listUsersURL,
-    listUsers
-  );
-
-  const listRoles = async () => {
-    permissionManager.OpenAPI.TOKEN = await getAccessTokenSilently();
-    permissionManager.OpenAPI.BASE = API_ROUTE.PERMISSION.BASE;
-    const listRolesRes = permissionManager.RoleService.listRoles({
-      perPage: 0,
-    });
-    return listRolesRes;
-  };
-
-  const listRolesQuery = `?per_page=0`;
-  const listRolesURL = `${API_ROUTE.PERMISSION.BASE}/roles${listRolesQuery}`;
-
-  const { data: listRolesRes, error: listRolesError } = useSWR(
-    listRolesURL,
-    listRoles
-  );
-
-  const updateUser: UserListProps["onUpdateUser"] = (target) => {
-    // update cache by local data
-    const prev = listUsersRes;
-    const newLocalUsers = prev?.users.map((user) => {
-      if (user.user_id !== target.user_id) {
-        return user;
-      } else {
-        return target;
-      }
-    });
-    mutate(listUsersURL, { ...prev, users: newLocalUsers }, false);
-
-    try {
-      mutate(listUsersURL, async () => {
-        try {
-          permissionManager.OpenAPI.TOKEN = await getAccessTokenSilently();
-          permissionManager.OpenAPI.BASE = API_ROUTE.PERMISSION.BASE;
-          const newTarget = await permissionManager.UserService.updateUser({
-            userId: target.user_id,
-            requestBody: {
-              role_ids: target.roles.map(
-                (role) => role.role_id as permissionManager.RoleModel["role_id"]
-              ),
-            },
-          });
-          const newUsers = prev?.users.map((user) =>
-            user.user_id !== newTarget.user_id ? user : newTarget
-          );
-          return { ...prev, users: newUsers };
-        } catch (error) {
-          setError({
-            reason: JSON.stringify(error),
-            instruction: "please reload this page",
-          });
-          return undefined;
-        }
-      });
-    } catch (error) {
-      setError({
-        reason: JSON.stringify(error),
-        instruction: "please reload this page",
-      });
-    }
-  };
-
-  useEffect(() => {
-    addQueryString({ page, perPage, searchText }, "replace");
-  }, [page, perPage, searchText]);
-
+const Component = ({
+  classes,
+  error,
+  searchText,
+  perPage,
+  perPageOptions,
+  isFetchComplete,
+  users,
+  roles,
+  totalPage,
+  page,
+  onChangeSearchText,
+  onChangePerPage,
+  onSaveUser,
+  onChangePage,
+}: Props) => {
   return (
     <>
       <PageToolBar
         right={
           <>
             <SearchForm
-              onSearch={(newSearchText) => setSearchText(newSearchText)}
+              onSearch={onChangeSearchText}
               defaultValue={searchText}
             />
             <Spacer direction="horizontal" size="15px" />
             <PerPageSelect
               perPage={perPage}
-              setPerPage={setPerPage}
-              values={[10, 20, 50, 100]}
+              setPerPage={onChangePerPage}
+              values={perPageOptions}
             />
             <Spacer direction="horizontal" size="15px" />
             <Button
@@ -159,40 +83,139 @@ const Container = (): JSX.Element => {
           </>
         }
       />
-      {listRolesRes?.roles?.length === 0 ? (
-        <div>
-          <strong>No roles defined! Please define role!</strong>
-        </div>
-      ) : null}
       <PageMain>
-        {listUsersError || listRolesError ? (
-          <ErrorMessage
-            reason={JSON.stringify(listUsersError || listRolesError)}
-            instruction="please reload this page"
-          />
-        ) : error ? (
+        {error ? (
           <ErrorMessage reason={error.reason} instruction={error.instruction} />
-        ) : listUsersRes && listRolesRes ? (
-          <UserList
-            users={listUsersRes.users}
-            roles={listRolesRes.roles}
-            onUpdateUser={updateUser}
-          />
+        ) : isFetchComplete && users && roles ? (
+          <UserList users={users} roles={roles} onUpdateUser={onSaveUser} />
         ) : (
           <LoadingIndicator />
         )}
       </PageMain>
       <Spacer direction="vertical" size="1vh" />
-      {listUsersRes ? (
-        <div className={styles.paginationContainer}>
+      {isFetchComplete ? (
+        <div className={classes.paginationContainer}>
           <Pagination
-            count={Math.ceil(listUsersRes.total / listUsersRes.per_page)}
+            count={totalPage}
             page={page}
-            onChange={(_, newPage) => setPage(newPage)}
+            onChange={(_, newPage) => onChangePage(newPage)}
           />
         </div>
       ) : null}
     </>
+  );
+};
+
+const useStyles = makeStyles(() => ({
+  paginationContainer: {
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "center",
+  },
+}));
+
+const Container = (): JSX.Element => {
+  const { getAccessTokenSilently: getAccessToken } = useAuth0();
+  const classes = useStyles();
+
+  // TODO: save per_page to local state
+  const [searchText, setSearchText] = useState(
+    getQueryString("searchText") || ""
+  );
+  const [perPage, setPerPage] = useState(
+    Number(getQueryString("perPage")) || 20
+  );
+  const [page, setPage] = useState(Number(getQueryString("page")) || 1);
+  const [error, setError] = useState<undefined | ErrorMessageProps>(undefined);
+
+  const [listUsersRes, listUsersError, listUsersCacheKey] = useListUsers(
+    getAccessToken,
+    {
+      page,
+      perPage,
+      search: searchText,
+    }
+  );
+  const [listRolesRes, listRolesError] = useListRoles(getAccessToken, {
+    perPage: 0,
+  });
+
+  const fetchError = listUsersError || listRolesError;
+  useEffect(() => {
+    if (fetchError) {
+      setError({
+        reason: JSON.stringify(fetchError),
+        instruction: "Please reload this page",
+      });
+    }
+  }, [fetchError]);
+  useEffect(() => {
+    addQueryString({ page, perPage, searchText }, "replace");
+  }, [page, perPage, searchText]);
+
+  const onSaveUser: UserListProps["onUpdateUser"] = (target) => {
+    // update cache by local data
+    const prev = listUsersRes;
+    const newLocalUsers = prev?.users.map((user) =>
+      user.user_id === target.user_id ? target : user
+    );
+    mutate(listUsersCacheKey, { ...prev, users: newLocalUsers }, false);
+
+    const updateUser = async () => {
+      const [updateUserRes, updateUserError] = await fetchPermissionManager(
+        getAccessToken,
+        permissionManager.UserService.updateUser,
+        {
+          userId: target.user_id,
+          requestBody: {
+            role_ids: target.roles.map(
+              (role) => role.role_id as permissionManager.RoleModel["role_id"]
+            ),
+          },
+        }
+      );
+
+      if (updateUserError) {
+        setError({
+          reason: JSON.stringify(updateUserError),
+          instruction: "Please reload this page",
+        });
+        return undefined;
+      } else if (updateUserRes) {
+        const newUsers = prev?.users.map((user) =>
+          user.user_id === updateUserRes?.user_id ? updateUserRes : user
+        );
+
+        return { ...prev, users: newUsers };
+      }
+      return undefined;
+    };
+
+    mutate(listUsersCacheKey, updateUser);
+  };
+
+  const isFetchComplete = Boolean(!fetchError && listUsersRes && listRolesRes);
+  const totalCount = listUsersRes
+    ? Math.ceil(listUsersRes.total / listUsersRes.per_page)
+    : 0;
+
+  return (
+    <Component
+      classes={classes}
+      error={error}
+      isFetchComplete={isFetchComplete}
+      onChangePage={setPage}
+      onChangePerPage={setPerPage}
+      onChangeSearchText={setSearchText}
+      onSaveUser={onSaveUser}
+      page={page}
+      perPage={perPage}
+      perPageOptions={[10, 20, 50, 100]}
+      roles={listRolesRes?.roles}
+      users={listUsersRes?.users}
+      searchText={searchText}
+      totalPage={totalCount}
+    />
   );
 };
 
