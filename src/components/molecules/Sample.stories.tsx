37c72529a90e3561e8e3c8909ef514771364e78f
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-08-30 11:00:42 +0900

    Introduce visual regression test

--- src/components/molecules/Sample.stories.tsx
+++ src/components/molecules/Sample.stories.tsx
@@ -1,12 +1,18 @@
 import { Story } from "@storybook/react";
 import { Sample, SampleProps } from "./Sample";
+import { TestAuthProvider, CONST_STORY_BOOK } from "test-utils";
 
 export default {
   component: Sample,
   title: "Sample",
 };
 
-const Template: Story<SampleProps> = (args) => <Sample {...args} />;
+const Template: Story<SampleProps> = (args) => (
+  <TestAuthProvider>
+    <Sample {...args} />
+  </TestAuthProvider>
+);
 
 export const Default = Template.bind({});
 Default.args = { sample: "sample" };
+Default.parameters = { ...CONST_STORY_BOOK.PARAM_SKIP_VISUAL_REGRESSION_TEST };
