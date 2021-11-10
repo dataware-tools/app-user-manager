2d134d2ef836e478c2c6b7edb4027da418994f75
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-11-10 17:29:14 +0900

    Fix docker related files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,18 +4,18 @@
 #
 
 # Fix Auth0's configurations
-[[ -z "${NEXT_PUBLIC_DATAWARE_TOOLS_AUTH_CONFIG_DOMAIN}" ]] \
-  && NEXT_PUBLIC_DATAWARE_TOOLS_AUTH_CONFIG_DOMAIN="dataware-tools.us.auth0.com"
-[[ -z "${NEXT_PUBLIC_DATAWARE_TOOLS_AUTH_CONFIG_CLIENT_ID}" ]] \
-  && NEXT_PUBLIC_DATAWARE_TOOLS_AUTH_CONFIG_CLIENT_ID="ETb1RhJEbtXlFgWtaHzl5kPCkaYqhTVl"
-[[ -z "${NEXT_PUBLIC_DATAWARE_TOOLS_AUTH_CONFIG_API_URL}" ]] \
-  && NEXT_PUBLIC_DATAWARE_TOOLS_AUTH_CONFIG_API_URL="https://demo.dataware-tools.com/"
-[[ -z "${NEXT_PUBLIC_DATAWARE_TOOLS_AUTH_MANAGE_PAGE}" ]] \
-  && NEXT_PUBLIC_DATAWARE_TOOLS_AUTH_MANAGE_PAGE="https://manage.auth0.com/dashboard/us/dataware-tools/users"
+[[ -z "${DATAWARE_TOOLS_AUTH_CONFIG_DOMAIN}" ]] &&
+  DATAWARE_TOOLS_AUTH_CONFIG_DOMAIN="dataware-tools.us.auth0.com"
+[[ -z "${DATAWARE_TOOLS_AUTH_CONFIG_CLIENT_ID}" ]] &&
+  DATAWARE_TOOLS_AUTH_CONFIG_CLIENT_ID="ETb1RhJEbtXlFgWtaHzl5kPCkaYqhTVl"
+[[ -z "${DATAWARE_TOOLS_AUTH_CONFIG_API_URL}" ]] &&
+  DATAWARE_TOOLS_AUTH_CONFIG_API_URL="https://demo.dataware-tools.com/"
+[[ -z "${DATAWARE_TOOLS_AUTH_MANAGE_PAGE}" ]] &&
+  DATAWARE_TOOLS_AUTH_MANAGE_PAGE="https://manage.auth0.com/dashboard/us/dataware-tools/users"
 
-sed -i -e "s/ETb1RhJEbtXlFgWtaHzl5kPCkaYqhTVl/${NEXT_PUBLIC_DATAWARE_TOOLS_AUTH_CONFIG_CLIENT_ID}/g" .next/static/chunks/**/*.js
-sed -i -e "s/dataware-tools.us.auth0.com/${NEXT_PUBLIC_DATAWARE_TOOLS_AUTH_CONFIG_DOMAIN}/g" .next/static/chunks/**/*.js
-sed -i -e "s|https://demo.dataware-tools.com/|${NEXT_PUBLIC_DATAWARE_TOOLS_AUTH_CONFIG_API_URL}|g" .next/static/chunks/**/*.js
-sed -i -e "s|https://manage.auth0.com/dashboard/us/dataware-tools/user|${NEXT_PUBLIC_DATAWARE_TOOLS_AUTH_MANAGE_PAGE}|g" .next/static/chunks/**/*.js
+sed -i -e "s/ETb1RhJEbtXlFgWtaHzl5kPCkaYqhTVl/${DATAWARE_TOOLS_AUTH_CONFIG_CLIENT_ID}/g" /app/dist/**/*.js
+sed -i -e "s/dataware-tools.us.auth0.com/${DATAWARE_TOOLS_AUTH_CONFIG_DOMAIN}/g" /app/dist/**/*.js
+sed -i -e "s|https://demo.dataware-tools.com/|${DATAWARE_TOOLS_AUTH_CONFIG_API_URL}|g" /app/dist/**/*.js
+sed -i -e "s|https://manage.auth0.com/dashboard/us/dataware-tools/user|${DATAWARE_TOOLS_AUTH_MANAGE_PAGE}|g" /app/dits/**/*.js
 
 exec "$@"
