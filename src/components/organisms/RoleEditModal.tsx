d9070afc3339c25723af449caf706b830076846a
Toshimitsu Watanabe <kz86nu@gmail.com>
2022-01-12 15:17:18 +0900

    Refactoring & Fix to confirm closing modal when changed data exists

--- src/components/organisms/RoleEditModal.tsx
+++ src/components/organisms/RoleEditModal.tsx
@@ -1,4 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
+
+import { metaStore } from "@dataware-tools/api-meta-store-client";
 import { permissionManager } from "@dataware-tools/api-permission-manager-client";
 import {
   LoadingIndicator,
@@ -11,23 +13,44 @@ import {
   DialogWrapper,
   usePrevious,
   extractReasonFromFetchError,
+  DialogBody,
+  DialogToolBar,
+  DialogMain,
+  NoticeableLetters,
+  DialogSubTitle,
+  confirm,
 } from "@dataware-tools/app-common";
+import LoadingButton from "@mui/lab/LoadingButton";
+import Box from "@mui/material/Box";
 import Dialog from "@mui/material/Dialog";
+import TextField from "@mui/material/TextField";
+import equal from "fast-deep-equal";
 import { useState, useEffect } from "react";
+import { useForm, Controller, Control, ControllerProps } from "react-hook-form";
 import { useSWRConfig } from "swr";
-import { RoleEditModalBody, RoleEditModalBodyProps } from "./RoleEditModalBody";
+import { PermissionList, PermissionListProps } from "./PermissionList";
 import { useGetRole, useListActions, useListDatabases } from "utils";
 
+type ValidateRuleType = ControllerProps["rules"];
+export type FormType = {
+  name: string;
+  description: string;
+  permissions: PermissionListProps["permissions"];
+};
 export type RoleEditModalPresentationProps = {
   error?: ErrorMessageProps;
-  roleEditModalBodyProps: RoleEditModalBodyProps;
   isFetchComplete: boolean;
+  actions: permissionManager.ActionModel[];
+  databases: metaStore.DatabaseModel[];
+  onSave: () => void;
+  formControl: Control<FormType>;
+  validateRules: Record<keyof FormType, ValidateRuleType>;
+  isSaving: boolean;
 } & Omit<RoleEditModalProps, "focusTarget" | "onSaveSucceeded" | "roleId">;
 
 export type RoleEditModalProps = {
   open: boolean;
-  focusTarget?: "roleName";
-  onClose: () => void;
+  onClose: (reason?: string) => void;
   onSaveSucceeded: (
     newRole: permissionManager.RoleModel
   ) => Promise<void> | void;
@@ -38,11 +61,21 @@ export const RoleEditModalPresentation = ({
   open,
   onClose,
   error,
-  roleEditModalBodyProps,
   isFetchComplete,
+  isSaving,
+  actions,
+  databases,
+  formControl,
+  onSave,
+  validateRules,
 }: RoleEditModalPresentationProps): JSX.Element => {
   return (
-    <Dialog open={open} onClose={onClose} maxWidth="xl" fullWidth>
+    <Dialog
+      open={open}
+      onClose={(_, reason) => onClose(reason)}
+      maxWidth="xl"
+      fullWidth
+    >
       <DialogWrapper>
         <DialogCloseButton onClick={onClose} />
         <DialogContainer padding="0 0 20px">
@@ -52,7 +85,83 @@ export const RoleEditModalPresentation = ({
               instruction={error.instruction}
             />
           ) : isFetchComplete ? (
-            <RoleEditModalBody {...roleEditModalBodyProps} />
+            <DialogBody>
+              <Controller
+                control={formControl}
+                rules={validateRules.name}
+                name="name"
+                render={({ field, fieldState: { error } }) => (
+                  <TextField
+                    {...field}
+                    variant="standard"
+                    required
+                    fullWidth
+                    InputProps={{
+                      sx: {
+                        fontSize: "1.7rem",
+                        fontWeight: "bolder",
+                        lineHeight: 1.5,
+                      },
+                    }}
+                    placeholder="Name"
+                    error={Boolean(error)}
+                    helperText={error?.message}
+                  />
+                )}
+              />
+              <Spacer direction="vertical" size="1vh" />
+              <DialogMain>
+                <div>
+                  <DialogSubTitle>
+                    <NoticeableLetters>Description</NoticeableLetters>
+                  </DialogSubTitle>
+                  <Box
+                    sx={{
+                      padding: "0 3vw",
+                    }}
+                  >
+                    <Controller
+                      control={formControl}
+                      rules={validateRules.description}
+                      name="description"
+                      render={({ field }) => (
+                        <TextField {...field} fullWidth multiline />
+                      )}
+                    />
+                  </Box>
+                </div>
+                <Spacer direction="vertical" size="1vh" />
+                <Controller
+                  control={formControl}
+                  rules={validateRules.permissions}
+                  name="permissions"
+                  render={({ field: { value, onChange } }) => (
+                    <PermissionList
+                      title={
+                        <DialogSubTitle>
+                          <NoticeableLetters>Permission</NoticeableLetters>
+                        </DialogSubTitle>
+                      }
+                      permissions={value}
+                      actions={actions}
+                      databases={databases}
+                      onChange={onChange}
+                    />
+                  )}
+                />
+              </DialogMain>
+              <DialogToolBar
+                right={
+                  <LoadingButton
+                    size="large"
+                    onClick={onSave}
+                    loading={isSaving}
+                  >
+                    Save
+                  </LoadingButton>
+                }
+              />
+            </DialogBody>
           ) : (
             <LoadingIndicator />
           )}
@@ -63,26 +172,17 @@ export const RoleEditModalPresentation = ({
   );
 };
 
+const initialDummyRole = { permissions: [], name: "", description: "" };
 export const RoleEditModal = ({
   open,
-  focusTarget,
-  onClose,
+  onClose: propsOnClose,
   onSaveSucceeded,
   roleId,
 }: RoleEditModalProps): JSX.Element => {
   const { getAccessTokenSilently: getAccessToken } = useAuth0();
   const { mutate } = useSWRConfig();
   const [error, setError] = useState<undefined | ErrorMessageProps>(undefined);
-
-  const initializeState = () => {
-    setError(undefined);
-  };
-  const prevOpen = usePrevious(open);
-  useEffect(() => {
-    if (open && !prevOpen) {
-      initializeState();
-    }
-  }, [open, prevOpen]);
+  const [isSaving, setIsSaving] = useState(false);
 
   const [listActionsRes, listActionsError] = useListActions(getAccessToken, {});
   const [listDatabasesRes, listDatabasesError] = useListDatabases(
@@ -93,6 +193,11 @@ export const RoleEditModal = ({
     getAccessToken,
     { roleId }
   );
+
+  const { control, handleSubmit, reset, getValues } = useForm<FormType>({
+    defaultValues: getRoleRes || initialDummyRole,
+  });
+
   const fetchError = listActionsError || listDatabasesError || getRoleError;
   const isFetchComplete = Boolean(
     !fetchError &&
@@ -101,16 +206,41 @@ export const RoleEditModal = ({
       (roleId ? getRoleRes : true)
   );
 
-  const onSaveRole = async (
-    role:
-      | permissionManager.UpdateRoleRequest
-      | permissionManager.CreateRoleRequest
-  ) => {
-    mutate(getRoleCacheKey, role, false);
-    const requestBody = {
-      name: role.name,
-      description: role.description,
-      permissions: role.permissions,
+  // When modal is opened, state should be initialized
+  const prevOpen = usePrevious(open);
+  useEffect(() => {
+    if (open && !prevOpen) {
+      setError(undefined);
+      reset(initialDummyRole);
+    }
+  }, [open, prevOpen, reset]);
+
+  // When succeed fetch, form value should be reset
+  useEffect(() => {
+    reset(getRoleRes);
+  }, [getRoleRes, reset]);
+
+  // When fail fetch , error should be shown
+  useEffect(() => {
+    if (fetchError) {
+      setError({
+        reason: extractReasonFromFetchError(fetchError),
+        instruction: "Please reload this page",
+      });
+    }
+  }, [fetchError]);
+
+  const onSaveRole = handleSubmit(async (formValues) => {
+    setIsSaving(true);
+    mutate(getRoleCacheKey, formValues, false);
+
+    const newRole = {
+      name: formValues.name,
+      description: formValues.description,
+      permissions: formValues.permissions.map((permission) => ({
+        databases: permission.databases,
+        action_ids: permission.actions.map((action) => action.action_id),
+      })),
     };
 
     permissionManager.OpenAPI.TOKEN = await getAccessToken();
@@ -125,45 +255,72 @@ export const RoleEditModal = ({
     const saveRoleRes = roleId
       ? await permissionManager.RoleService.updateRole({
           roleId: roleId,
-          requestBody: requestBody,
+          requestBody: newRole,
         }).catch(setError)
       : await permissionManager.RoleService.createRole({
-          requestBody: requestBody,
+          requestBody: newRole,
         }).catch(setError);
 
     if (saveRoleRes) {
       onSaveSucceeded(saveRoleRes);
       mutate(getRoleCacheKey);
+      setIsSaving(false);
+      propsOnClose();
     }
+  });
 
-    return Boolean(saveRoleRes);
+  const onClose = async (reason?: string) => {
+    const confirmClosingModal = async () => {
+      return await confirm({
+        title: "Are you sure you want close this dialog?",
+        body: "Changed data will not be saved",
+        confirmText: "Close",
+        confirmMode: "delete",
+      });
+    };
+
+    switch (reason) {
+      case "backdropClick":
+      case "escapeKeyDown":
+        if (roleId && !equal(getRoleRes, getValues())) {
+          if (!(await confirmClosingModal())) {
+            return;
+          }
+        } else if (
+          !roleId &&
+          Object.values(getValues()).some(
+            (value) => value !== "" && value != null
+          )
+        ) {
+          if (!(await confirmClosingModal())) {
+            return;
+          }
+        }
+        propsOnClose();
+        break;
+
+      default:
+        propsOnClose();
+    }
   };
 
-  useEffect(() => {
-    if (fetchError) {
-      setError({
-        reason: extractReasonFromFetchError(fetchError),
-        instruction: "Please reload this page",
-      });
-    }
-  }, [fetchError]);
-
-  const roleEditModalBodyProps: RoleEditModalPresentationProps["roleEditModalBodyProps"] =
-    {
-      actions: listActionsRes?.actions || [],
-      databases: listDatabasesRes?.data || [],
-      onModalClose: onClose,
-      onSave: onSaveRole,
-      focusTarget: focusTarget,
-      initialRole: getRoleRes,
-    };
+  const validateRules: RoleEditModalPresentationProps["validateRules"] = {
+    name: { required: "Role name is required" },
+    description: {},
+    permissions: {},
+  };
   return (
     <RoleEditModalPresentation
       open={open}
       error={error}
       isFetchComplete={isFetchComplete}
-      roleEditModalBodyProps={roleEditModalBodyProps}
       onClose={onClose}
+      isSaving={isSaving}
+      actions={listActionsRes?.actions || []}
+      databases={listDatabasesRes?.data || []}
+      formControl={control}
+      validateRules={validateRules}
+      onSave={onSaveRole}
     />
   );
 };
